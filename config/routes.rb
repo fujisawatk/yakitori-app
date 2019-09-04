222fef745ddb66a101148e84de0e08919241d6f3
fujisawatk <fuji19880707@gmail.com>
2019-09-05 01:34:25 +0900

    add omniauth function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root "infos#index"
   resources :infos, only: [:index]
 end
