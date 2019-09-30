25735efa9eac9514bde586c0dfd57d0476aa654f
fujisawatk <fuji19880707@gmail.com>
2019-09-30 20:57:48 +0900

    add categories and relations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_012950) do
+ActiveRecord::Schema.define(version: 2019_09_30_115204) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -20,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_09_29_012950) do
     t.datetime "updated_at", null: false
     t.string "img"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_categories_on_category_id"
+    t.index ["post_id"], name: "index_posts_categories_on_post_id"
   end
 
   create_table "restaurants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,5 +62,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_012950) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "posts_categories", "categories"
+  add_foreign_key "posts_categories", "posts"
   add_foreign_key "restaurants", "posts"
 end
