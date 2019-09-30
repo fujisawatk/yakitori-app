02cc2c18ed55285ca6c119de52d74600e5593a59
fujisawatk <fuji19880707@gmail.com>
2019-09-30 21:17:54 +0900

    add association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one :restaurant, dependent: :destroy
   accepts_nested_attributes_for :restaurant
+  has_many :categories, through: :post_categories
+  has_many :post_categories
 
   mount_uploader :img, ImgUploader
 end
