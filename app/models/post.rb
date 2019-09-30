1b953b031b2ee9bf626d8018701deddbc1379288
fujisawatk <fuji19880707@gmail.com>
2019-09-30 23:08:56 +0900

    fix relation
    - rename relation table
    - fix association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one :restaurant, dependent: :destroy
   accepts_nested_attributes_for :restaurant
-  has_many :categories, through: :post_categories
-  has_many :post_categories
+  has_many :post_category_relations
+  has_many :categories, through: :post_category_relations
 
   mount_uploader :img, ImgUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_120259) do
+ActiveRecord::Schema.define(version: 2019_09_30_132620) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,13 +18,13 @@ ActiveRecord::Schema.define(version: 2019_09_30_120259) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "post_category_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id"
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_post_categories_on_category_id"
-    t.index ["post_id"], name: "index_post_categories_on_post_id"
+    t.index ["category_id"], name: "index_post_category_relations_on_category_id"
+    t.index ["post_id"], name: "index_post_category_relations_on_post_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2019_09_30_120259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "post_categories", "categories"
-  add_foreign_key "post_categories", "posts"
+  add_foreign_key "post_category_relations", "categories"
+  add_foreign_key "post_category_relations", "posts"
   add_foreign_key "posts", "users"
   add_foreign_key "restaurants", "posts"
 end
