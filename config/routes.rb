608b00d39100a75ed23dd6fe55e74e2c4026b1d9
fujisawatk <fuji19880707@gmail.com>
2019-10-03 23:33:03 +0900

    add comment function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root "posts#index"
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
 
   resources :users, only: :show do
     member do
