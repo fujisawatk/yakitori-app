ff2dbf8d73f71190bc5b30b83615ba932ce04d71
fujisawatk <fuji19880707@gmail.com>
2019-10-05 20:36:20 +0900

    add validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   belongs_to :post
+
+  validate :restaurant_check
+
+  private
+    def restaurant_check
+      errors.add(:name, 'を登録してください') if !name.present?
+    end
+
 end
