6a888eb3a02671cde0b483fe5de6f3880cdfdac8
fujisawatk <fuji19880707@gmail.com>
2019-10-05 11:59:43 +0900

    add constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_03_123657) do
+ActiveRecord::Schema.define(version: 2019_10_05_024717) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
