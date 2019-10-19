328951d33a35b4d6a6f21a098d23ff2643094907
fujisawatk <fuji19880707@gmail.com>
2019-10-19 20:58:05 +0900

    del gon

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,9 @@
 class CategoriesController < ApplicationController
-  include SetGon
 
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
     @posts = @category.posts.order(created_at: :desc).page(params[:page]).per(8)
-    set_gon
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-  include SetGon
 
   def mylist
     @user = User.find(params[:id])
     @categories = Category.all
     @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(8)
-    set_gon
   end
 
 end
