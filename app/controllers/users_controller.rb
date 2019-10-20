db7b260c625a310ebda5f9d18d9a9944dbf45cb9
fujisawatk <fuji19880707@gmail.com>
2019-10-20 21:28:50 +0900

    add mycomment and myliked view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
-
+  before_action :set_params
+  
   def mylist
+    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(8)
+  end
+
+  def mycomment
+    @comment_posts = @user.comments.order(created_at: :desc)
+    @array_posts = @comment_posts.map(&:post).flatten
+    @posts = Kaminari.paginate_array(@array_posts).page(params[:page]).per(8)
+  end
+
+  def myliked
+    @liked_posts = @user.likes.order(created_at: :desc)
+    @array_posts = @liked_posts.map(&:post).flatten
+    @posts = Kaminari.paginate_array(@array_posts).page(params[:page]).per(8)
+  end
+
+  private
+
+  def set_params
     @user = User.find(params[:id])
     @categories = Category.all
-    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(8)
   end
 
 end
