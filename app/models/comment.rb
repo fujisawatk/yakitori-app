37d7b9c86256ff3352baa383d21c25ece1640b79
fujisawatk <fuji19880707@gmail.com>
2019-10-27 23:16:23 +0900

    fix column name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, length: { maximum: 200 }, presence: true
+  validates :comment, length: { maximum: 200 }, presence: true
 end
